539a8bd1b70d0406ee4f89de016d664af10a301b
Yihang Li <yihang3@student.unimelb.edu.au>
2024-07-10 20:00:57 +1000

    Create q3.py

---
+++ .ipynb_checkpoints/q3.py
@@ -0,0 +1,19 @@
+import numpy as np
+def max_weight_subset(nums):
+    candidates = []
+    max_weight = -999999
+    for i in nums:
+        candidates.append([i])
+    count = 0
+    while len(candidates) > 0:
+        subset = candidates.pop(0)
+        weight = np.mean(subset) - np.median(subset)
+        if weight > max_weight:
+            max_weight = weight
+        for i in candidates:
+            new = list(set(subset + i))
+            if new not in candidates:
+                candidates.append(new)
+    return max_weight
+
+print(max_weight_subset([1,2,3,4]))
